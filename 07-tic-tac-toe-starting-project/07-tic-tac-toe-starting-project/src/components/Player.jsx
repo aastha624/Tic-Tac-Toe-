0c30321aef5cb364cd8a5569017f9431dfad7e56
aastha624 <patelaastha2910@gmail.com>
2024-02-28 23:37:16 +0530

    upadating stte base on new state and user input and two way binding

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,25 +1,39 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [ isEditing, setIsEditing ] = useState(false);
+  const [playername,setplayername]=useState(initialName);
 
-  function handleEditClick() {
-    setIsEditing(true);
+  function handlechange(event){
+    console.log(event);
+    // this is new concept and put the name and fix the name tame je name rakhavu hoi ae rakhi ne save kari ne fix kari sako onchane and handlechange and another state  no concept
+    setplayername(event.target.value);
+
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleEditClick() {
+    // setIsEditing(!isEditing);--use state upadating function
+    setIsEditing((editing)=>!editing);
+  }
 
+  let editableplayername = <span className="player-name">{playername}</span>;
+  // let btncaption='Edit';
   if (isEditing) {
-    playerName = <input type="text" required />;
+    editableplayername = (
+    <input type="text" required value={playername} onChange={handlechange} />
+    //show that name as a input----conditional content and A Suboptimal Way Of Upadating State
+    // btncaption='save' ;
+    //another method use ternary expression .
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editableplayername}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing? 'Save' : 'Edit'}</button>
     </li>
   );
 }
