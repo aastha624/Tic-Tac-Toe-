fdb53b6060430b96bbab5f7d419212d9ad7a2d40
aastha624 <patelaastha2910@gmail.com>
2024-03-10 22:54:26 +0530

    prefer-computed-values and disabling-buttons

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,17 +3,27 @@ import Player from './components/Player.jsx';
 import Gameboard from './components/Gameboard.jsx';
 import Log from './components/Log.jsx';
 
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activeplayer,setactiveplayer]=useState('x');
-  function handleselectsquare(rowindex,colindex){
-    setactiveplayer((curactiveplayer)=>(curactiveplayer==='X'?'O':'X'));
-    setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
+  // const [activeplayer,setactiveplayer]=useState('x');
 
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+  const activePlayer = deriveActivePlayer(gameTurns);
+  function handleselectsquare(rowindex,colindex){
+    // setactiveplayer((curactiveplayer)=>(curactiveplayer==='X'?'O':'X'));
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowindex, col: colindex }, player: currentPlayer },
@@ -23,17 +33,17 @@ function App() {
       return updatedTurns;
     });
   
-}
+  }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player" symbol="X" isactive={activeplayer ==='X'}/>
-          <Player initialName="Player" symbol="O" isactive={activeplayer ==='O'} />
+          <Player initialName="Player 1" symbol="X" isactive={activeplayer ==='X'}/>
+          <Player initialName="Player 2" symbol="O" isactive={activeplayer ==='O'} />
         </ol>
         <Gameboard onselectsquare={handleselectsquare} turns={gameTurns}/>
       </div>
-      <Log />
+      <Log  turns={gameTurns} />
     </main>
   );
 }
--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Gameboard.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Gameboard.jsx
@@ -8,42 +8,42 @@ const intialGameboard = [
 
 // RENDERING MULTIDIMENTIONAL LISTING--not write code in hard code write the code in dyanamically
 
-export default function Gameboard({onselectsquare}) {
-// derived state
-    let gameboard =intialGameboard;
-    for (const turn of turns){
-const {square,player}=turn;
-const{row,col}=square;
-gameboard[row][col]=player;
+export default function Gameboard({ onselectsquare }) {
+    // derived state
+    let gameboard = intialGameboard;
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+        gameboard[row][col] = player;
     }
-// export default function Gameboard({onselectsquare,activeplayersymbol}) {
-//     const[gameboard,setgameboard]=useState(intialGameboard);
+    // export default function Gameboard({onselectsquare,activeplayersymbol}) {
+    //     const[gameboard,setgameboard]=useState(intialGameboard);
 
-//     function handleselectsquare(rowindex,colindex){
-//         setgameboard((prevGameBoard)=>{
-//             const updateBoard=[...prevGameBoard.map(innerArray=>[...innerArray])];
-//             // achieving old array using map method----using this aaproach updating the state immutable way --handle function assign onclick and create new anonumus function
-// updateBoard[rowindex][colindex]=activeplayersymbol;
-// return updateBoard;  
-//         });
+    //     function handleselectsquare(rowindex,colindex){
+    //         setgameboard((prevGameBoard)=>{
+    //             const updateBoard=[...prevGameBoard.map(innerArray=>[...innerArray])];
+    //             // achieving old array using map method----using this aaproach updating the state immutable way --handle function assign onclick and create new anonumus function
+    // updateBoard[rowindex][colindex]=activeplayersymbol;
+    // return updateBoard;  
+    //         });
     //     onselectsquare();
     // }
     return (
-    <ol id="game-board">
-        {/* dynamically render those grid items */}
-        {gameboard.map((row, rowindex) => (<li key={rowindex}>
+        <ol id="game-board">
+            {/* dynamically render those grid items */}
+            {gameboard.map((row, rowindex) => (<li key={rowindex}>
 
-            <ol>
-                {row.map((playername, colindex) => (<li key={colindex}>
-                    {/* <button> {playername}</button> */}
-                    {/* <button onClick={()=>handleselectsquare(rowindex,colindex)}>{playername}</button> */}
-                    <button onClick={()=>onselectsquare(rowindex,colindex)}>{playername}</button> 
-                </li>
-                ))}
-            </ol>
-        </li>
-        ))}
-    </ol>
+                <ol>
+                    {row.map((playername, colindex) => (<li key={colindex}>
+                        {/* <button> {playername}</button> */}
+                        {/* <button onClick={()=>handleselectsquare(rowindex,colindex)}>{playername}</button> */}
+                        <button onClick={() => onselectsquare(rowindex, colindex)} disabled={playerSymbol !== null}> {playername} </button>
+                    </li>
+                    ))}
+                </ol>
+            </li>
+            ))}
+        </ol>
     );
-                }
+}
 
