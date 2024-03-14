155fad40ed05d9d0041107572bc66819232e0899
aastha624 <patelaastha2910@gmail.com>
2024-03-14 23:24:28 +0530

    gameover checking for draw

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Player from './components/Player.jsx';
 import Gameboard from './components/Gameboard.jsx';
+import GameOver from './components/Gameover.jsx';
 import Log from './components/Log.jsx';
 import {WINNING_COMBINATIONS} from "./components/winning-combinations.js";
 
@@ -25,13 +26,36 @@ function App() {
   const activeplayer = deriveActivePlayer(gameTurns);
 
   let gameboard = intialGameboard;
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-        gameboard[row][col] = player;
-    }
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameboard[row][col] = player;
+  }
 
-  function handleselectsquare(rowindex,colindex){
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameboard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameboard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+     gameboard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) 
+    {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+
+  function handleselectsquare(rowindex, colindex) {
     // setactiveplayer((curactiveplayer)=>(curactiveplayer==='X'?'O':'X'));
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
@@ -43,18 +67,19 @@ function App() {
 
       return updatedTurns;
     });
-  
+
   }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isactive={activeplayer ==='X'}/>
-          <Player initialName="Player 2" symbol="O" isactive={activeplayer ==='O'} />
+          <Player initialName="Player 1" symbol="X" isactive={activeplayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isactive={activeplayer === 'O'} />
         </ol>
-        <Gameboard onselectsquare={handleselectsquare} turns={gameTurns}/>
+        {(winner || hasDraw) && <GameOver winner={winner} />}
+        <Gameboard onselectsquare={handleselectsquare} board={gameboard} />
       </div>
-      <Log  turns={gameTurns} />
+      <Log turns={gameTurns} />
     </main>
   );
 }
